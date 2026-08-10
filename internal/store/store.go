677d373d89a04f9e8eda4e634eee290f22d7438e
Martin Fontanet <martin.fontanet@epfl.ch>
2026-08-10 23:08:58 +0200

    feat: move and link files on add

--- internal/store/store.go
+++ internal/store/store.go
@@ -10,6 +10,10 @@ import (
 	"github.com/oklog/ulid/v2"
 )
 
+const (
+	tmpDirPattern = "rido-park-*"
+)
+
 type Store struct {
 	Items []StoreItem
 }
@@ -48,5 +52,59 @@ func WriteStoreItem(storeItem *StoreItem) error {
 		return err
 	}
 
+	err = moveAndLink(*storeItem.Meta, storeItemFolder)
+	if err != nil {
+		return fmt.Errorf("could not move and link file: %w", err)
+	}
+
+	return nil
+}
+
+func moveAndLink(meta Meta, dstFolder string) error {
+	// TODO:
+	// - [x] Copy
+	// - [ ] Verify
+	// - [x] Park
+	// - [x] Link
+	// - [x] Remove
+	dstFile := filepath.Join(dstFolder, meta.Filename)
+
+	err := fs.CopyFile(dstFile, meta.Origin)
+	if err != nil {
+		return fmt.Errorf("failed to copy file: %w", err)
+	}
+
+	// TODO: Verify checksum.
+
+	dir, err := os.MkdirTemp("", tmpDirPattern)
+	if err != nil {
+		return fmt.Errorf("failed to create temp dir: %w", err)
+	}
+	defer os.RemoveAll(dir)
+
+	parkedFile := filepath.Join(dir, meta.Filename)
+	if e := os.Rename(meta.Origin, parkedFile); e != nil {
+		return fmt.Errorf("failed to move %q to %q: %w", meta.Origin, parkedFile, e)
+	}
+
+	cleanup := func() error {
+		if e := os.Rename(parkedFile, meta.Origin); e != nil {
+			return fmt.Errorf("failed to restore parked file: %w", e)
+		}
+
+		return nil
+	}
+
+	err = os.Symlink(dstFile, meta.Origin)
+	if err != nil {
+		e := cleanup()
+		if e != nil {
+			err = fmt.Errorf("%w; cleanup failed: %w", err, e)
+		}
+
+		return fmt.Errorf("failed to create symlink: %w", err)
+	}
+
+	// No need to manually remove `parkedFile`: it is done automatically by the deferred call to `os.RemoveAll`.
 	return nil
 }
